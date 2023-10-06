a81ffcaa48b882d669da66292fb7a8e55e18f628
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-06 03:43:57 +0000

    learn variable

---
+++ basics/variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553 // Declare of constant values 
+let accountEmail = "satish123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jamui" // default set variable type 
+
+let Name; // in JavaScript throw 'undefined' because not value in the variable
+
+
+
+
+
+// accountId = 2 //Not Allowed
+console.log(accountId)
+
+/*
+    prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,Name])
