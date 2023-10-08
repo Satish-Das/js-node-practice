23d0f0a76869cb0c21f17d5d3196f67ac2067a43
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-08 04:27:49 +0000

    Comparision wtih datatypes

---
+++ basics/comparison.js
@@ -0,0 +1,17 @@
+// this is equalty check in your given datatypes
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1)
+console.log("02" > 1) 
+
+// not use such types of conversion
+// console.log(null > 0)
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
