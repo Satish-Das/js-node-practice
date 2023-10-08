230fe62c076e230827939faf00f06a5541b5d31e
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-08 04:02:04 +0000

    Conversion of dateTypes

---
+++ basics/conversionOp.js
@@ -0,0 +1,24 @@
+let score = '33'
+
+// console.log(score)
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+let booleanValues = false
+console.log(Number(booleanValues));
+
+let booleanIsLogged = true
+console.log(Number(booleanIsLogged));
+
+// 1 => true; 0 => false
+// " " => false
+// "hitesh" => true
