45573ecdf5efa6bd1d3712463feeba5833927694
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-08 03:45:56 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert("hello") // We are using node js
+
+let name = "Satish"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+
+// bigint 
+// String => " "
+// boolean => true / false
+// null => standalone value
+// undefine => 
+// Symbol => unique
+
+// object 
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
