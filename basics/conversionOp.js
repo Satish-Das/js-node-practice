8514a78a0eeb4aa53fe6434092d7a29179b213ff
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-08 04:17:59 +0000

    Strings

--- basics/conversionOp.js
+++ basics/conversionOp.js
@@ -6,19 +6,62 @@ let score = '33'
 // "33abs" => NaN
 // true => 1; false => 0
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
-let booleanValues = false
-console.log(Number(booleanValues));
+// let booleanValues = false
+// console.log(Number(booleanValues));
 
-let booleanIsLogged = true
-console.log(Number(booleanIsLogged));
+// let booleanIsLogged = true
+// console.log(Number(booleanIsLogged));
 
 // 1 => true; 0 => false
 // " " => false
 // "hitesh" => true
+
+
+
+// ************************** Operation **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Satish"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(2 + 4* 4/6 % 6)
+
+// console.log(+true) => 1
+// console.log(+""); //=> 0
+
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+gameCounter = 100 
+// console.log(gameCounter++); //postFix
+
+gameCounter1 = 100
+// console.log(++gameCounter1); // prefix
