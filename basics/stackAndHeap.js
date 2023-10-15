59799bee297d92de3ff90cc36c374c513c352c03
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-15 08:55:03 +0000

    memory

---
+++ basics/stackAndHeap.js
@@ -0,0 +1,24 @@
+// Stack And heap
+
+
+// Stack (primitive)
+
+// Heap (Non-Primitive)
+
+let myYoutubename = "satish "
+
+let anotherName = myYoutubename
+
+anotherName = "chai or code"
+console.log(anotherName)
+
+
+// Heap Memory
+let userOne = {
+    Email: "satish123@gmail.com",
+    upi: "user2ypl"
+}
+// Here using reference 
+let userTwo = userOne
+
+userTwo.Email= "satish234@google.com"
