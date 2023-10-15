94605bf97516901afe430353584bf666948ac8e6
Satish das <116028837+satishkumar09x@users.noreply.github.com>
2023-10-15 09:21:27 +0000

    Strings

--- basics/str.js
+++ basics/str.js
@@ -1,4 +1,23 @@
 const name = "Satish"
 const repoCount = 50
+// dont use
+// console.log(name + repoCount + " Value")
 
-console.log(name + repoCount + " Value")
+// console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String('satish');
+
+Name1 = "Satish"
+console.log(Name1.toUpperCase());
+console.log(Name1.toLowerCase());
+
+// check which postion of char
+console.log(Name1.charAt(2)); 
+console.log(Name1.indexOf('t'));
+
+const newString = gameName.substring(-3,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-4,5)
+console.log(anotherString);
+
