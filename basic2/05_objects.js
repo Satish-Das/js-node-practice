5010e0322242b470cb38ee0a1b074b8c4ddddcb0
Satish Das <116028837+Satish-Das@users.noreply.github.com>
2023-11-04 16:43:49 +0000

    some learn object

---
+++ basic2/05_objects.js
@@ -0,0 +1,13 @@
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor: "DasBabu"
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+const {courseInstructor:instructor} = course
+// console.log(courseInstructor)
+// console.log(instructor);
+
