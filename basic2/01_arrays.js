8269eb1dc3b1f811b855b9b8664e83f98eab5a80
Satish Das <116028837+Satish-Das@users.noreply.github.com>
2023-11-04 15:30:34 +0000

    learn basic method of arrays

---
+++ basic2/01_arrays.js
@@ -0,0 +1,36 @@
+// Arrays is a object because have many data 
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["saktiman", "spider Man"]
+// console.log(myArr.length);// access to 0 index 
+
+// Arrays methods
+
+// myArr.push(8); // adding data in array in last position 
+// console.log(myArr);
+
+// myArr.pop() // remove date in array from last element
+// console.log(myArr);
+
+// myArr.unshift(9); // Add element in array at first index
+// console.log(myArr);
+
+// myArr.shift(); // Remove element in array at first index
+// myArr.shift();
+// console.log(myArr)
+// console.log(myArr.includes(4))// Checking given element in array have or Not
+// console.log(myArr.indexOf(4))// check given element of index values 
+
+// const newArr = myArr.join()
+// console.log(newArr)
+
+// slice, splice 
+// console.log("A ", myArr)
+// const myn1 = myArr.slice(1, 3) // skip the late given Value
+// console.log(myn1);
+
+// console.log("B ", myArr)
+// const myn2 = myArr.splice(1, 3) // count 1 to 3 all are print 
+// console.log(myn2);
+
