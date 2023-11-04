61f01f04403eff26b40c4c1b0ac564cc9ad55bf3
Satish Das <116028837+Satish-Das@users.noreply.github.com>
2023-11-04 05:56:23 +0000

    learn Date

---
+++ basics/Date_function.js
@@ -0,0 +1,19 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+
+// let myCreatedDate = new Date(2023,10, 4)
+// let myCreatedDate = new Date(2023,10, 4, 11, 13)
+let myCreatedDate = new Date("2023-11-04")
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
