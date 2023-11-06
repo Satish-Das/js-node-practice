35fe13cac9b33619b2877964fd3ac9494ade3f13
Satish Das <116028837+Satish-Das@users.noreply.github.com>
2023-11-06 05:29:00 +0000

    learn loop and more things

---
+++ 05_iteration/one.js
@@ -0,0 +1,45 @@
+// for 
+
+// for (let index = 0; index < 10; index++){
+//     const element = index;
+//     console.log(element);
+// }
+
+
+// for (let index = 0; index < 10; index++){
+//     const element = index;
+//     console.log(element);
+//     if(element == 5){
+//         console.log("5 Is the best number.");
+//     }
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop Value: ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+// }
+
+
+// let arr = ["Flash", "Batman", "SuperMan"]
+// console.log(arr.length);
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
+// }
+
+
+// break and continue 
+// for (let index = 1; index < 20; index++) {
+//     const element = index;
+//     if(index == 5){
+//         continue; //continue keyword
+//     }
+//     else if(index == 10){
+//         break; // break keyword
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
