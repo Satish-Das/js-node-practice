55cc9275e0d3c6e074cd963c15659e91c04e1b6e
Satish Das <116028837+Satish-Das@users.noreply.github.com>
2023-11-06 05:40:48 +0000

    Learned while or Do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,32 @@
+// While and do while loop 
+// while (condition) {
+    
+// }
+let index = 0;
+
+// while (index  <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index++
+// }
+
+
+// let myArray = ["Flash","batman","Superman"]
+// let arr = 0
+// while(arr <= myArray.length){
+//     console.log(`Vlaue is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// ----------------------------- do while loop ----------------------------
+// do {
+    
+// } while (condition);
+
+
+let sccore =1
+
+do{
+    console.log(`score is ${sccore}`);
+    sccore ++
+}while(sccore <=10)
